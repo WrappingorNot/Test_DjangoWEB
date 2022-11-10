34777ee3213b763bdccfbb77469429bc7739578c
ddincddong <88953636+ddincddong@users.noreply.github.com>
2022-11-10 17:06:37 +0900

    generic View, 정적(Static) 파일사용

--- app_default/admin.py
+++ app_default/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+
 # Register your models here.
+
+class ChoiceInline(admin.TabularInline):
+     model = Choice
+     extra = 5
+
+class QuestionAdmin(admin.ModelAdmin):
+     fieldsets = [
+          (None,         {'fields': ['question_text']}),
+          ('Date information', {'fields':['pub_date']}),
+     ]
+     inlines = [ChoiceInline]
+admin.site.register(Question, QuestionAdmin)
