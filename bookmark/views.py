1bd90ddaf04a3938d6b15b7178832c94654e9607
ddincddong <88953636+ddincddong@users.noreply.github.com>
2022-11-15 17:01:21 +0900

    Bookmarkapp 추가 기능 구현

--- bookmark/views.py
+++ bookmark/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
+from django.views.generic.detail import DetailView
 from django.urls import reverse_lazy
 from .models import Bookmark
 
@@ -15,4 +16,6 @@ class BookmarkCreateView(CreateView):
      fields = ['site_name', 'url']
      success_url = reverse_lazy('list')
      template_name_suffix = '_create'
-     
+
+class BookmarkDetailView(DetailView):
+     model = Bookmark
