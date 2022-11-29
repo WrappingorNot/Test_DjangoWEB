087d9fbca112cf6b9083fa0c14f383131c46ab92
coco0826@sju.ac.kr <coco0826@sju.ac.kr>
2022-11-29 11:10:35 +0900

    Mysql연동성공

--- config/settings.py
+++ config/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "BLOG",
+        "USER":"root",
+        "PASSWORD":"Ehowl203040@",
+        "HOST":"localhost",
+        "PORT":"3306"
     }
 }
 
